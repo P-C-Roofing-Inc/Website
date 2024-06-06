a25bb0e6eca6e1e0449fb4ae16733eb78f6d19fd
Fernando Barajas <fbarajas015@gmail.com>
2024-06-05 20:15:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////Gallery code/////////////////////////////
+
+const panels = document.querySelectorAll(".panel");
+
+function removeActiveClasses() {
+panels.forEach((panel) => {
+    panel.classList.remove("active");
+});
+}
+panels.forEach((panel) => {
+panel.addEventListener("click", () => {
+    if (panel.classList.contains("active")) {
+    removeActiveClasses();
+    } else {
+    removeActiveClasses();
+    panel.classList.add("active");
+    }
+});
+});
