24c0903f345c852f4d0d73b346871e8ec109bcf9
barajasfe <fbarajas015@gmail.com>
2024-11-03 14:45:47 -0800

    Changed a reviews and main color

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 const panels = document.querySelectorAll(".panel");
+const changePicButton = document.getElementById("change_picture");
+const TPOGallery = document.images.item.getElementById("roof_1");
 
 function removeActiveClasses() {
 panels.forEach((panel) => {
@@ -9,10 +11,25 @@ panels.forEach((panel) => {
 panels.forEach((panel) => {
 panel.addEventListener("click", () => {
     if (panel.classList.contains("active")) {
-    removeActiveClasses();
+        TPOGallery.src = "IMG_1144.jpg";
+        removeActiveClasses();
     } else {
     removeActiveClasses();
     panel.classList.add("active");
     }
 });
 });
+
+changePicButton.addEventListener("mouseenter", function(){
+    var buttonColor = changePicButton.style.background.toString;
+    changePicButton.style.border = "1.9px dotted orange";
+
+})
+
+changePicButton.addEventListener("mouseleave", function(){
+    changePicButton.style.border = "1px solid #333";
+
+})
+changePicButton.addEventListener("click", function(){
+    TPOGallery.style.display = "none";
+;});
